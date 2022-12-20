a5ee57227420d8abcd02943ecbd4bc72af4813fb
Tarık Talha DİNÇ <tariktalhadinc@gmail.com>
2022-12-20 12:31:35 +0300

    Axaj Call
    ajax call added

--- PersonalService/Program.cs
+++ PersonalService/Program.cs
@@ -1,7 +1,9 @@
 global using Microsoft.EntityFrameworkCore;
 global using PersonalService.Data;
+using Microsoft.AspNetCore.Builder;
 using PersonalService.Interfaces;
 using PersonalService.Services;
+using System.Threading.Tasks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +20,17 @@ builder.Services.AddTransient<IPersonalSystem, PersonalSystem>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins();
+        });
+});
+
+
+
 
 var app = builder.Build();
 
@@ -27,6 +40,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(builder => builder
+.AllowAnyHeader()
+.AllowAnyMethod()
+.SetIsOriginAllowed(_ => true)
+.AllowCredentials()
+);
 
 app.UseHttpsRedirection();
 
